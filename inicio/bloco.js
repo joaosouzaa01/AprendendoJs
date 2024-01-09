707fbb23dbb1f000a8a4cd7dd24be1bf2b24088b
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-09 20:42:25 -0300

    Aprendendo sobre Blocos de código

---
+++ inicio/bloco.js
@@ -0,0 +1,15 @@
+// UM CODIGO QUE TEM 5 SENTENÇAS E 2 BLOCOS
+
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+    console.log("Passo #06");
+}
+
+
