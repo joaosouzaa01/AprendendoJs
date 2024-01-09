2c23afd43e8525ffe73fd47b77c42132dc3b546e
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-09 19:45:15 -0300

    Sentença de codigo

---
+++ inicio/sentença_2.js
@@ -0,0 +1,6 @@
+// Isto é uma sentença de codigo
+console.log("Passo 1#"); // Sentença de codigo número 1
+console.log("Passo 2#"); // Sentença de codigo número 2
+console.log("Passo 3#"); // Sentença de codigo número 3
+
+// O console.log - imprime algo no console
