c54b76d54adf2fcafee310b9b2de3958ca240042
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-10 19:35:37 -0300

    Aprendendo sobre variaveis

---
+++ inicio/Dados/dados_1.js
@@ -0,0 +1,13 @@
+var produto = "caneta";
+var marca = "BIC";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5 
+let precoFinal = preco + imposto      
+
+console.log(produto);
+console.log("Marca =",marca);
+console.log("Quantidade =",quantidade); //VAlor Literal
+console.log("Preço = R$",preco);
+console.log("Imposto = R$",imposto); 
+console.log("Valor final = R$",precoFinal); 
