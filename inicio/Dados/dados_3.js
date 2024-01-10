0baddfa774eb80515152136c0914d38c6d082ddf
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-10 19:51:16 -0300

    Tipos de dados e variaveis

---
+++ inicio/Dados/dados_3.js
@@ -0,0 +1,12 @@
+let idade = 21;
+    console.log(typeof idade);
+    console.log(typeof 31);
+
+let salario = 4500.21;
+    console.log(typeof salario);
+
+var estaChovendo = true //OU FALSE
+    console.log(typeof estaChovendo);
+
+console.log(typeof 'Test');    
+console.log(typeof "Test");    
