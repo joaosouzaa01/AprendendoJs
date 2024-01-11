93334fb5100711aa574e5589170d125c59ef072e
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-11 19:58:33 -0300

    Update desafio_circunferencia.js

--- inicio/Desafios/desafio_circunferencia.js
+++ inicio/Desafios/desafio_circunferencia.js
@@ -1,6 +1,6 @@
     
     const PI = 3.14159;
     let raio = 10;
-    let area = PI * (raio * raio);
+    let areaCirc = PI * (raio * raio);
 
-    console.log("O valor da área é: " + area + " m2");
+    console.log("O valor da área é: " + areaCirc + "m2");
