ac94fa04f01c4d918eca9b235538ebec6cac6a32
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-11 20:08:06 -0300

    Update desafio_troca.js

---
+++ inicio/Desafios/desafio_troca.js
@@ -0,0 +1,15 @@
+let a = 7 ;
+let b = 94;
+
+    console.log("O valor de A é: "+ a);
+    console.log("O valor de B é: "+ b);
+
+let temp = a;
+
+    a = b ;
+    b = temp ;
+
+        console.log("O valor de A depois da troca é: " + a);    
+        console.log("O valor de B depois da troca é: " +b);
+
+
