dbfe48aac209a236c921c8ceefd3582fe4638ab5
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-11 19:37:28 -0300

    Adicionando comentarios

--- inicio/Dados/dados_4.js
+++ inicio/Dados/dados_4.js
@@ -1,2 +1,12 @@
 let a = 4
 
+// Você consegue manipular a variavel (let) de qualquer forma
+
+//mudando o valor no decorrer do codigo, ex.
+a = 8
+
+// ou adicionando mais atributos ex.
+
+// a = a + 12
+
+// Quando o valor for alterado no decorrer do codigo, o que vai valer é o ultimo Atruibuido, no caso a linha 10
