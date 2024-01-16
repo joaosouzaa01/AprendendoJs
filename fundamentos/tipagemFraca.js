126311169e68fa11eb0fc6a2043544ef237e1ace
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-16 18:54:33 -0300

    Update tipagemFraca.js

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,19 @@
+let qualquer = 'Legal!';
+
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.14;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+/*
+ O JAVASCRIPT É UMA LINGUAGEM DE TIPAGEM FRACA
+ -EVITE NOMES GENÉRICOS E SIGLAS EX:
+*/
+
+let valor = ''
+
+let numero =1
+
+let pqp = false // SIGLA PARA PRODUTO QUÍMICO PERIGOSO ... KKK
