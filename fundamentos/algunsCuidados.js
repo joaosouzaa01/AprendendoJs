a55a82d362dd09d13d4f70cbb93d332ba1e143ef
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-16 20:02:16 -0300

    Revert "Revert "Create algunsCuidados.js""
    This reverts commit 7ebcc8ae5d04e13464889761785a3228ae28066a.

---
+++ fundamentos/algunsCuidados.js
@@ -0,0 +1,8 @@
+console.log (7 / 0)
+console.log ( "10 / 2")
+console.log ( "show!" / 2)
+console.log ( 0.1 + 0.7)
+
+console.log ((10.545). toFixed(2))
+
+// NaN SIGNIFICA QUE NENHUM NÚMERO FOI DECLARADO
