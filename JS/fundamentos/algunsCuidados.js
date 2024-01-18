8650dc481fb7ddcd871d5586bf6de3c447138457
João Victor <teixeiralopesdesouzaj@gmail.com>
2024-01-18 19:36:21 -0300

    Update algunsCuidados.js

--- JS/fundamentos/algunsCuidados.js
+++ JS/fundamentos/algunsCuidados.js
@@ -1,8 +1,11 @@
 console.log (7 / 0)
 console.log ( "10 / 2")
+console.log ( '3' + 2) // O RESULTA MOSTRA QUE A OPERAÇÃO NÃO FOI FEITA,  POIS OUVE UMA CONCATENAÇÃO NO CODIGO, NESTE CASO A STRING GANHA
+console.log ( '3' - 2)
 console.log ( "show!" / 2)
 console.log ( 0.1 + 0.7)
 
 console.log ((10.545). toFixed(2))
+
 
 // NaN SIGNIFICA QUE NENHUM NÚMERO FOI DECLARADO
