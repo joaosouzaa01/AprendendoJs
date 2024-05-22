fcabaf0048c65b48d3302a5f86ba9f2c73123944
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-05-22 20:06:27 -0300

    Create usandoVar1.js

---
+++ JS/fundamentos/usandoVar1.js
@@ -0,0 +1,23 @@
+{
+    {
+        {
+            {
+                {
+                    var sera = "Será ??? "
+                        console.log(sera)
+                }
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+    function teste () {
+        var local = 1234
+        console.log (local)
+    }
+
+    
+teste()
+console.log(local)    
