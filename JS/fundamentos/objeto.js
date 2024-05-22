96ef1b2f528467ab9097057aaacb7c007718877d
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-05-22 20:06:46 -0300

    Create objeto.js

---
+++ JS/fundamentos/objeto.js
@@ -0,0 +1,14 @@
+const prod1 = {}
+    prod1.nome = 'Celular ultra mega!'
+    prod1.preco = 4990.90
+prod1['Desconto Legal! '] = 0.50 // EVITAR ATRIBUTOS COM ESPAÇOS
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco : 79.90
+}
+
+console.log(prod2)
+
