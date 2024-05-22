fcce9c6b3f11549320c7df3cfbcaf222404da739
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-05-22 20:07:11 -0300

    Create atribuicao.js

---
+++ JS/fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7 
+let b = 5
+
+b += a // b = b + a
+console.log(b)
+
+b -= a // b = b - a
+console.log(b)
+
+b *= a // b = b * a
+console.log(b)
+
+b /= a // b = b / a
+console.log(b)
+
+b %= a // b = b % a
+console.log(b)
