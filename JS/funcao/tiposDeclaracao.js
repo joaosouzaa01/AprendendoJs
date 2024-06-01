4e7bd754014e45f8d959b7a37e6a89470ce48420
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-06-01 11:52:53 -0300

    Create tiposDeclaracao.js

---
+++ JS/funcao/tiposDeclaracao.js
@@ -0,0 +1,21 @@
+console.log(soma(3, 4))
+
+
+//FUNCTION DECLARATION
+    function soma (x, y) {
+        return x + y
+    }
+
+//OBRIGADOS A CHAMAR A FUNÇÃO DEPOIS  DA DECLARAÇÃO
+    
+    //FUNCTION EXPRESSION
+        const sub = function (x, y) {
+            return x - y
+        }
+        console.log(soma(3, 4))
+
+    //NAMED FUNCTION EXPRESSION
+        const mult = function mult (x, y) {
+            return x * y
+        }
+        console.log(soma(3, 4))
