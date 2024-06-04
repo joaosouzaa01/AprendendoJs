901ef5ecbd17fbcc55878267a1a27891a06ec11f
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-06-04 20:07:55 -0300

    Update oo.js

--- JS/objeto/oo.js
+++ JS/objeto/oo.js
@@ -4,4 +4,15 @@
 //Função que manipula dados
     processamento(valor1, valor2, valor3,)
 
+// OO
+//Dados que contém Funções
+    objeto = {
+        valor1, 
+        valor2,
+        valor3,
+        processamento() {
+            //...
+        }
+    }
 
+    objeto.processamento() // Foco passou a ser o objeto
