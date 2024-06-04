a21a052c6f84d2d7fbe8a08bc9b1007285ddf4c0
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-06-04 20:08:01 -0300

    Update oo.js

--- JS/objeto/oo.js
+++ JS/objeto/oo.js
@@ -16,3 +16,9 @@
     }
 
     objeto.processamento() // Foco passou a ser o objeto
+
+// Principios importantes
+    // 1. Abstração
+    // 2. Encapsulamento
+    // 3. Herança (prototype)
+    // 4. Prototype 
