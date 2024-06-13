f62689010d592c4ebe3da9049f506e06360b22ee
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-06-12 21:03:51 -0300

    Update evitandoModificacoes.js

--- JS/objeto/evitandoModificacoes.js
+++ JS/objeto/evitandoModificacoes.js
@@ -3,3 +3,22 @@
 const produto = Object.preventExtensions({
   nome: 'Monitor Mancer', preco: 1299.9, tag: 'promoção'
 })
+console.log('Extensível:', Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.descricao = 'Borracha escolar branca'
+delete produto.tag
+
+console.log(produto)
+
+//Object.seal
+const pessoa = { nome: 'Juliana', idade: 25 }
+Object.seal(pessoa)
+console.log('Selado: ', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+console.log(pessoa)
+
+//object.freeze = selado + valores constantes
