cf3ace5b04780255f090102c1619517c5f7880b6
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-06-13 20:35:38 -0300

    Update jsonVsObj.js

--- JS/objeto/jsonVsObj.js
+++ JS/objeto/jsonVsObj.js
@@ -4,3 +4,4 @@ console.log(JSON.stringify(obj))
 // console.log(JSON.parse("{a: 1, b: 2, c: 3}")) 
 // console.log(JSON.parse("{'a1: 1, 'b': 2, 'c': 3}"))
 console.log(JSON.parse('{"a1": 1, "b": 2, "c": 3}')) // Maneira correta de ser declarada
+console.log(JSON.parse('{"a": 1.7, "b":"string", "c": "true", "d": {}, "e": [] }'))
