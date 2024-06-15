be56e2cac2ac524c86de1c54206ce27bd762f303
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-06-15 18:11:21 -0300

    Update classe2.js

--- JS/objeto/classe2.js
+++ JS/objeto/classe2.js
@@ -7,6 +7,14 @@ class Avo {
 class Pai extends Avo {
   constructor(sobrenome, profissao = 'Professor') {
     super(sobrenome)
-    
+    this.profissao = profissao
   }
 }
+
+class Filho extends Pai {
+  constructor() {
+    super('Silva')
+  }
+}
+const filho = new Filho
+console.log(filho) 
