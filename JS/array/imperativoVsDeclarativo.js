500593958f196b8c39671ecb8af517be258706ef
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-06-18 19:03:32 -0300

    Update imperativoVsDeclarativo.js

--- JS/array/imperativoVsDeclarativo.js
+++ JS/array/imperativoVsDeclarativo.js
@@ -2,3 +2,10 @@ const alunos = [
   { nome: 'João', nota: 7.9 },
   { nome: 'Maria', nota: 8.9 }
 ]
+
+// IMPERATIVO
+let total1 = 0
+for (let i = 0; i < alunos.length; i++) {
+  total1 += alunos[i].nota
+}
+console.log(total1 / alunos.length)
