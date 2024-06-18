6dd8d8e1df8b40e5cb84dc39496962c8833a38e3
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-06-18 19:28:52 -0300

    Create flatMap.js

---
+++ JS/array/flatMap.js
@@ -0,0 +1,38 @@
+const escola = [{
+  noma: 'Turma M1',
+  alunos: [{
+    nome: 'Gustavo',
+    nota: 8.7
+  }, {
+    nome: 'Henrique',
+    nota: 7.8
+  }, {
+    nome: 'Agatha',
+    nota: 9.5
+  }]
+}, {
+  nome: 'Turma M2',
+  alunos: [{
+    nome: 'Alice',
+    nota: 5.7
+  }, {
+    nome: 'Luiz',
+    nota: 8.4
+  }, {
+    nome: 'Kauã',
+    nota: 8.7
+  }]
+}]
+
+const getNotaDoAluno = aluno => aluno.nota
+const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
+
+const notas1 = escola.map(getNotasDaTurma)
+console.log(notas1)
+
+Array.prototype.flatMap = function(callback) {
+  return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const notas2 = escola.flatMap(getNotasDaTurma)
+console.log(notas2)
