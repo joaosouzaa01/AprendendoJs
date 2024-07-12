607e8c94bafae3522da09699b224895a6f6d03a0
João Victor <88406996+joaosouzaa01@users.noreply.github.com>
2024-07-11 22:56:19 -0300

    create database

--- projeto/src/servidor.js
+++ projeto/src/servidor.js
@@ -2,9 +2,34 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
-  res.send({ nome: 'Notebook', preco: 1230.99 }) // Converter para JSON
+  res.send(bancoDeDados.getProdutos())
+})
+
+app.get('./produtos/:id', (req, res, next) => {
+  res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('./Produtos', (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.nome,
+    preco: req.body.preco
+  })
+  res.send(produto) //JSON
+})
+
+app.put('./produtos/:id', (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    id:req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco
+  })
+  res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
